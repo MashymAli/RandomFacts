9d769377056aa1dc15e4d204c1ed178ee716d627
Mashym Ali <mashymalii@gmail.com>
2022-12-07 23:23:11 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+$.ajax({
+    method: 'GET',
+    url: 'https://api.api-ninjas.com/v1/facts?',
+    headers: { 'X-Api-Key': 'mKTluWUB0ES2cAKQ/uIoxg==guJICSFT5FKfYIHk'},
+    contentType: 'application/json',
+    success: function(result) {
+        console.log(result['0'].fact);
+    },
+    error: function ajaxError(jqXHR) {
+        console.error('Error: ', jqXHR.responseText);
+    }
+});
+
