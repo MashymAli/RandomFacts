429b7833bb84f59163b406b4e69803596e55de9f
Mashym Ali <mashymalii@gmail.com>
2022-12-08 19:08:11 +0500

    api  update

--- JS/index.js
+++ JS/index.js
@@ -1,11 +1,11 @@
 let fact = document.getElementById("fact")
 let btn = document.getElementById("newfact")
-
+let api_key = process.env.API_KEY;
 function getfact() {
 $.ajax({
     method: 'GET',
     url: 'https://api.api-ninjas.com/v1/facts?',
-    headers: { 'X-Api-Key': 'mKTluWUB0ES2cAKQ/uIoxg==guJICSFT5FKfYIHk'},
+    headers: { 'X-Api-Key': api_key},
     contentType: 'application/json',
     success: function(result) {
         console.log(result['0'].fact);
