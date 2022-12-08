4aba7b8b52ce95e18344bffaf86b82cd130bb401
Mashym Ali <mashymalii@gmail.com>
2022-12-08 19:55:40 +0500

    done

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,6 @@
 let fact = document.getElementById("fact")
 let btn = document.getElementById("newfact")
-let api_key = process.env.api_key1;
+let api_key = '';
 function getfact() {
 $.ajax({
     method: 'GET',
